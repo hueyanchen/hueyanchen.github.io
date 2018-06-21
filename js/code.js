d50f863239c6972269b6ffd7241d34e354393d89
hueyanchen <35254525+hueyanchen@users.noreply.github.com>
2018-06-21 09:36:58 +0800

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -4,6 +4,20 @@ var rtl = (document.location.search == '?rtl');
 var block = null;
 
 function start() {
+   var modal = document.getElementById('myModal');
+   var span = document.getElementsByClassName("close")[0];
+   modal.style.display = "block";
+   // When the user clicks on <span> (x), close the modal
+   span.onclick = function() {
+       modal.style.display = "none";
+   }
+   // When the user clicks anywhere outside of the modal, close it
+   window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+  } 
+
   Blockly.inject(document.getElementById('blocklyDiv'), 
       {
         path: '../', 
